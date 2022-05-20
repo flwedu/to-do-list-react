d592fe77214aeff9294c8a2b7e57b7a02d47e0f1
Eduardo Aquino <edutraquino@gmail.com>
2022-05-19 21:22:15 -0300

    refact: set notes context to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from "react"
 import InputField from "./components/InputField"
 import NotesList from "./components/NotesList"
+import { NotesContextProvider } from "./providers/NotesProvider"
+import LocalStorageService from "./service/local-storage.js"
 
 function App() {
+  const storage = new LocalStorageService("notes")
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="heading">
-          <h1>To-Do List</h1>
-        </div>
-        <div className="body">
-          <InputField />
-          <NotesList />
+    <NotesContextProvider storage={storage}>
+      <div className="App">
+        <div className="container">
+          <div className="heading">
+            <h1>To-Do List</h1>
+          </div>
+          <div className="body">
+            <InputField />
+            <NotesList />
+          </div>
         </div>
       </div>
-    </div>
+    </NotesContextProvider>
   )
 }
 
