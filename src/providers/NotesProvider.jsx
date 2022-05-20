9702cc49bb638b41b8de15c1f266878303ff37ab
Eduardo Aquino <edutraquino@gmail.com>
2022-05-19 21:02:56 -0300

    feat: create context functions

--- src/providers/NotesProvider.jsx
+++ src/providers/NotesProvider.jsx
@@ -1,24 +1,50 @@
-import { createContext, useState } from "react"
-import NoteObject from "../model/NoteObject"
+import { createContext, useEffect, useState } from "react"
+import NoteObject from "../model/NoteObject.js"
+import LocalStorageService from "../service/local-storage.js"
 
 const NotesContext = createContext()
 /**
- * Exposts a provider to NotesContext
- * @param {{children: JSX.Element; storageService: LocalStorageService;}} param0
+ * Exposes a provider to NotesContext
+ * @param {{children: JSX.Element; storage: LocalStorageService;}} param0
  */
-export default function NotesContextProvider({ children, storageService }) {
+export default function NotesContextProvider({ children, storage }) {
   const [notes, setNotes] = useState([])
 
+  useEffect(() => {
+    storage.save(notes)
+  }, [notes])
+
   /**
-   *
-   * @param {NoteObject} note
+   * @param {string} text
    */
-  function addNote(note) {
+  function addNoteWithText(text) {
+    const note = new NoteObject(text)
     setNotes([...notes, note])
   }
 
+  /**
+   * @param {string} id
+   */
+  function switchNoteStatus(id) {
+    const note = notes.find((el) => el.id === id)
+    if (note) {
+      note.done = !note.done
+      setNotes(notes)
+      storage.save(notes)
+    }
+  }
+
+  /**
+   * @param {string} id
+   */
+  function deleteNote(id) {
+    setNotes(notes.filter((note) => note.id !== id))
+  }
+
   return (
-    <NotesContext.Provider value={{ notes, addNote }}>
+    <NotesContext.Provider
+      value={{ notes, addNoteWithText, switchNoteStatus, deleteNote }}
+    >
       {children}
     </NotesContext.Provider>
   )
